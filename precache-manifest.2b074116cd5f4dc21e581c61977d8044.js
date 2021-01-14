7362cbd9685c5ff72552ddc999ba5ead7d9abc63
haseeb943 <62321504+haseeb943@users.noreply.github.com>
2021-01-14 20:45:25 +0500

    Updates

---
+++ precache-manifest.2b074116cd5f4dc21e581c61977d8044.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffde95308050fe5f985bc57cbd811c50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed9e66b0ba9a73a758a",
+    "url": "/static/css/main.642b7388.chunk.css"
+  },
+  {
+    "revision": "cae97eaf6656eb56f227",
+    "url": "/static/js/2.d8fd58d6.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.d8fd58d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ed9e66b0ba9a73a758a",
+    "url": "/static/js/main.5169f11e.chunk.js"
+  },
+  {
+    "revision": "009d58c90185213bf65c",
+    "url": "/static/js/runtime-main.a0404f6a.js"
+  }
+]);
