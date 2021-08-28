37755699d56da9808212aa3ea70c9f8ed8e53851
Haseeb <ha796111@gmail.com>
2021-08-28 16:23:12 +0500

    Updates

---
+++ precache-manifest.7749d2d8ee9d859262d2e7fe07ac80a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7bb58c6f5bc90d6cd7ef7cb39867df",
+    "url": "/Netflix-Clone/index.html"
+  },
+  {
+    "revision": "0bfa7d0edf8b1bf21598",
+    "url": "/Netflix-Clone/static/css/main.642b7388.chunk.css"
+  },
+  {
+    "revision": "1652091df57714d02152",
+    "url": "/Netflix-Clone/static/js/2.16c6df2d.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix-Clone/static/js/2.16c6df2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bfa7d0edf8b1bf21598",
+    "url": "/Netflix-Clone/static/js/main.c4c5d8a9.chunk.js"
+  },
+  {
+    "revision": "90f71f2d424308d31b2a",
+    "url": "/Netflix-Clone/static/js/runtime-main.217d174b.js"
+  }
+]);
